e0ab567f0cbe7c6c904a0edbec1a5e227d24988b
Jimmi Robles <jimmirobles@icloud.com>
2019-10-19 11:07:02 -0500

    bugs solved

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,12 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav mr-auto">
-
+                    <li class="nav-item">
+                        <a href="{{ route('home') }}" class="nav-link active">Manuales</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('users') }}" class="nav-link">Usuarios</a>
+                    </li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
