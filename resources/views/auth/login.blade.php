021c1c4998d21a72316f5492a721ce3391860b38
Jimmi Robles <jimmirobles@icloud.com>
2019-10-25 21:24:21 -0500

    Complemento
    errores corregidos

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,8 @@
                             @honeypot
                             <div class="form-group row">
                                 <label for="email"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+                                       class="col-md-4 col-form-label text-md-right"
+                                       >{{ __('E-Mail') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email"
